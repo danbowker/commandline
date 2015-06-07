85de906657b23f689f47907371373169acced3ba
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-06-07 07:26:04 +0200

    Changes to tests

--- src/CommandLine.FSharp.Tests/ArgParserTests.fs
+++ src/CommandLine.FSharp.Tests/ArgParserTests.fs
@@ -2,24 +2,23 @@
 
     open CommandLine
 
-    type public FakeOptions() = class
+    type FakeOptions() =
         let mutable stringValue = ""
-//        let mutable intSequence = Seq.empty<int>
-//        let mutable boolValue = false
-//        let mutable longValue = 0L
+        let mutable intSequence = Seq.empty<int>
+        let mutable boolValue = false
+        let mutable longValue = 0L
 
         [<Option(HelpText = "Define a string value here.")>]
-        member this.StringValue with public get() = stringValue and public set(value) = stringValue <- value
+        member this.StringValue with get() = stringValue and set(value) = stringValue <- value
 
-//        [<Option('i', Min = 3, Max = 4, HelpText = "Define a int sequence here.")>]
-//        member this.IntSequence with get() = intSequence and set(value) = intSequence <- value
-//
-//        [<Option('x', HelpText = "Define a boolean or switch value here.")>]
-//        member this.BoolValue with get() = boolValue and set(value) = boolValue <- value
-//
-//        [<Value(0)>]
-//        member this.LongValue with get() = longValue and set(value) = longValue <- value
-    end
+        [<Option('i', Min = 3, Max = 4, HelpText = "Define a int sequence here.")>]
+        member this.IntSequence with get() = intSequence and set(value) = intSequence <- value
+
+        [<Option('x', HelpText = "Define a boolean or switch value here.")>]
+        member this.BoolValue with get() = boolValue and set(value) = boolValue <- value
+
+        [<Value(0)>]
+        member this.LongValue with get() = longValue and set(value) = longValue <- value
 
     module parse_all_options =
         open Xunit
